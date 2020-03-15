782c97959ab2f5cd2ce634062bb5356d93dd8442
Rahuldev1986 <alinadigitalservices@gmail.com>
2020-03-15 19:25:09 +0530

    All are updated

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+    jQuery('.humberger-menu').click(function(){
+        jQuery('.menu-cover').addClass('open');
+        jQuery('.menu-cover').removeClass('close');
+    });
+    jQuery('.cross').click(function(){
+        jQuery('.menu-cover').addClass('close');
+        jQuery('.menu-cover').removeClass('open');
+    });
+})
